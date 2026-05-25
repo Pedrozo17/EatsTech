c8425354df4737a575bdfea0f60fcd778574c254
Pedrozo17 <samuelpedrozobaena9@gmail.com>
2026-05-25 15:46:40 -0500

    ultima modificacion

--- config/Configuracion.php
+++ config/Configuracion.php
@@ -1,13 +1,14 @@
 <?php
-//DB details
-$dbHost = 'localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'carrito';
+// Parámetros de conexión para producción en Alwaysdata
+$db_host = "mysql-eatstech.alwaysdata.net"; // Revisa el host exacto en tu panel
+$db_user = "eatstech";               // El usuario de BD que creaste
+$db_pass = "Samuel0917";        // La contraseña que le asignaste
+$db_name = "eatstech_db";         // El nombre completo de tu BD
 
-//Create connection and select DB
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 if ($db->connect_error) {
-    die("No hay Conexion con la base de datos: " . $db->connect_error);
+    die("Fallo en la conexión de producción: " . $db->connect_error);
 }
+$db->set_charset("utf8");
+?>
--- config/con_db.php
+++ config/con_db.php
@@ -1,5 +1,14 @@
 <?php
+// Parámetros de conexión para producción en Alwaysdata
+$db_host = "mysql-eatstech.alwaysdata.net"; // Revisa el host exacto en tu panel
+$db_user = "eatstech";               // El usuario de BD que creaste
+$db_pass = "Samuel0917";        // La contraseña que le asignaste
+$db_name = "eatstech_db";         // El nombre completo de tu BD
 
-    $conex = mysqli_connect("localhost", "root", "", "registro"); 
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
+if ($db->connect_error) {
+    die("Fallo en la conexión de producción: " . $db->connect_error);
+}
+$db->set_charset("utf8");
 ?>
