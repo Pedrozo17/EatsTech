b772dc47781db12374cacacbe6514a16c159980b
Pedrozo17 <samuelpedrozobaena9@gmail.com>
2026-05-25 16:05:23 -0500

    poninedo los env y gitignore

--- config/Configuracion.php
+++ config/Configuracion.php
@@ -1,14 +1,18 @@
 <?php
-// Parámetros de conexión para producción en Alwaysdata
-$db_host = "mysql-eatstech.alwaysdata.net"; // Revisa el host exacto en tu panel
+// Detalles de la conexión de producción en Alwaysdata
+$db_host = "mysql-eatstech.alwaysdata.net";
 $db_user = "eatstech";               // El usuario de BD que creaste
-$db_pass = "S17";        // La contraseña que le asignaste
-$db_name = "eatstech_db";         // El nombre completo de tu BD
+$db_pass = "Samuel0917";             // La contraseña que le asignaste
+$db_name = "eatstech_db";
 
+// Creación de la conexión con las variables correctas
 $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
+// Validación de errores
 if ($db->connect_error) {
     die("Fallo en la conexión de producción: " . $db->connect_error);
 }
+
+// Configuración del juego de caracteres
 $db->set_charset("utf8");
 ?>
