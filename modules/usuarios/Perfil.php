ae751ae3026fb7385789d9b6c697e4b7ff0a3446
Pedrozo17 <samuelpedrozobaena9@gmail.com>
2026-06-11 12:45:21 -0500

    ya sirve el responsive de perfil

--- modules/usuarios/Perfil.php
+++ modules/usuarios/Perfil.php
@@ -66,6 +66,7 @@ $monitoreo_ordenes = $ordenes_query->get_result();
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Perfil - Camaron Express</title>
     <link rel="stylesheet" href="../../assets/css/perfil.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
